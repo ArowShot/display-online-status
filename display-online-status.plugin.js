87410446151f6159f12a44c99a436f960e6d68cc
Kyle <Arow411@gmail.com>
2018-03-25 00:04:43 -0400

    add code

---
+++ display-online-status.plugin.js
@@ -0,0 +1,21 @@
+//META{"name":"DisplayOnlineStatus"}*//
+
+class DisplayOnlineStatus {
+	constructor() {
+		
+	}
+	
+	getName () {return "DisplayOnlineStatus";}
+
+	getDescription () {return "displays the little online status dot next to people's names in chat like it does in the user list.";}
+
+	getVersion () {return "1.0.0";}
+
+	getAuthor () {return "ArowShot";}
+	
+	load() {}
+	
+	start() {
+		
+	}
+}
